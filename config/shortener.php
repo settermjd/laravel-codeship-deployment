6569f8097ea6dea96734f108d5af034609bf07d9
Matthew Setter <matthew@maltblue.com>
2016-07-25 11:14:40 +0200

    Add shortening support
    This is making use zenapply/laravel-shortener. This commit includes the
    core changes required to support that, which are the Composer change,
    along with the auto-generated and updated config file. For more
    information, check out the package on Packagist - https://packagist.org/packages/zenapply/laravel-shortener.

---
+++ config/shortener.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /**
+     * The url shortening service to use. 
+     * if set to NULL it will use all services in the accounts array
+     */
+    'driver' => env('SHORTENER_DRIVER','google'),
+
+    'cache' => [
+        /**
+         * Whether or not to use Laravels Cache driver
+         */
+        'enabled' => env('SHORTENER_CACHE_ENABLED',true),
+
+        /**
+         * The duration in minutes to remember the url in cache
+         */
+        'duration' => env('SHORTENER_CACHE_DURATION',1440),
+    ],
+
+    'accounts' => [
+        'google' => [
+            [
+                'token' => env('GOOGLE_SHORTENER_TOKEN_1'), 
+            ],
+            [
+                'token' => env('GOOGLE_SHORTENER_TOKEN_2'), 
+            ],
+            [
+                'token' => env('GOOGLE_SHORTENER_TOKEN_3'), 
+            ],
+        ],
+        'bitly' => [
+            [
+                'username' => env('BITLY_SHORTENER_USERNAME_1'), 
+                'password' => env('BITLY_SHORTENER_PASSWORD_1')
+            ],
+            [
+                'username' => env('BITLY_SHORTENER_USERNAME_2'), 
+                'password' => env('BITLY_SHORTENER_PASSWORD_2')
+            ],
+            [
+                'username' => env('BITLY_SHORTENER_USERNAME_3'), 
+                'password' => env('BITLY_SHORTENER_PASSWORD_3')
+            ],
+        ], 
+    ],
+];
