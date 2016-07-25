5b25a5c4307a6e13e8f0d68e7a721c1ff2239e2e
Matthew Setter <matthew@maltblue.com>
2016-07-25 11:16:45 +0200

    Add basic support for creating urls
    Here I'm implementing a VERY rudimentary controller action for creating
    URLs. No, it doesn't do any kind of validation or any other form of
    sanitisation or defensive coding. I'm well aware of that. But that will
    change over coming commits.

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Url;
 use Illuminate\Http\Request;
+use Zenapply\Shortener\Facades\Shortener;
 
 class UrlController extends Controller
 {
     public function manageUrl(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $url = new Url();
+            $url->original_url = $request->input('url');
+            $url->shortened_url = Shortener::shorten($request->input('url'));
+            $url->save();
+
+            return redirect('/view-urls');
+        }
+
         return view('url.manage', []);
     }
 
